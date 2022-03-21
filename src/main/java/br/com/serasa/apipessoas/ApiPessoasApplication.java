084ad77d97daebc4beee24a67d31bd798e2c336f
carlosjrs006 <carlosjrs06.cj@gmail.com>
2022-03-21 18:05:58 -0300

    🎉 Criação do projeto

---
+++ src/main/java/br/com/serasa/apipessoas/ApiPessoasApplication.java
@@ -0,0 +1,13 @@
+package br.com.serasa.apipessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/serasa/apipessoas/ApiPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serasa.apipessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
